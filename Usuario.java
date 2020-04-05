e0790e838b2800e34785df58e5df844e4789acf5
Nicolas Gonzalez <nicoogoonzalez@gmail.com>
2020-04-05 19:39:17 -0300

    Punto A

---
+++ Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareadosigenieriadesoftware;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Usuario {
+    
+    private String nombre;
+    private int cedula;
+    private String contraseña;
+    private int edad;
+    private int cantidadDeHijos;
+    private int saldo = 0;
+    
+    
+}
